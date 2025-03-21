139901368a439922572c5f3b55d45269d65375db
unknown <voronine@ukr.net>
2025-03-21 11:13:31 +0200

    configTable

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,15 +1,14 @@
 import React from 'react'; 
 import { Table, Row, Col, Layout } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { useFetchUsers } from '../../hooks/useFetchUsers';
 import { logout } from '../../redux/slices/aythSlice';
-import UserRow from './UserRow';
 import UserDetails from './UserDetails';
 import '../../styles/Dashboard.scss';
 import { User } from '../../types/User';
 import DashboardMenu from '../Menu/DashboardMenu';
+import { userTableColumns } from './userTableColumns';
 
 const { Header, Content } = Layout;
 
@@ -22,32 +21,6 @@ const Dashboard: React.FC = () => {
     localStorage.removeItem('user');
     dispatch(logout());
   };
-
-  const columns: ColumnsType<User> = [
-    {
-      title: 'First Name',
-      key: 'firstName',
-      sorter: (a: User, b: User) => a.firstName.localeCompare(b.firstName),
-      render: (_text: string, record: User) => (
-        <UserRow user={record} />
-      ),
-      width: '40%',
-    },
-    {
-      title: 'Last Name',
-      dataIndex: 'lastName',
-      key: 'lastName',
-      sorter: (a: User, b: User) => a.lastName.localeCompare(b.lastName),
-      width: '40%',
-    },
-    {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-      sorter: (a: User, b: User) => a.age - b.age,
-      width: '20%',
-    },
-  ];
 
   return (
     <Layout>
@@ -64,7 +37,7 @@ const Dashboard: React.FC = () => {
           <Col span={24}>
             <Table<User>
               dataSource={users}
-              columns={columns}
+              columns={userTableColumns}
               rowKey="id"
               loading={loading}
               pagination={{ pageSize: 10 }}
---
+++ src/components/Dashboard/userTableColumns.tsx
@@ -0,0 +1,27 @@
+import type { ColumnsType } from 'antd/es/table';
+import { User } from '../../types/User';
+import UserRow from './UserRow';
+
+export const userTableColumns: ColumnsType<User> = [
+  {
+    title: 'First Name',
+    key: 'firstName',
+    sorter: (a: User, b: User) => a.firstName.localeCompare(b.firstName),
+    render: (_text: string, record: User) => <UserRow user={record} />,
+    width: '40%',
+  },
+  {
+    title: 'Last Name',
+    dataIndex: 'lastName',
+    key: 'lastName',
+    sorter: (a: User, b: User) => a.lastName.localeCompare(b.lastName),
+    width: '40%',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    sorter: (a: User, b: User) => a.age - b.age,
+    width: '20%',
+  },
+];
